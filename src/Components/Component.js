e81aef070320792273b6814b9de690a9bd740f70
Shannon Scott <sean3rd0@icloud.com>
2019-10-31 13:42:33 -0600

    Fixed with Lucas' help.

--- src/Components/Component.js
+++ src/Components/Component.js
@@ -29,7 +29,7 @@ class Component extends React.Component{
     }
 
     handleAddFruitButtonClick = (addFruitInput) => {
-        this.props.addFruit(addFruitInput)
+        // this.props.addFruit(addFruitInput)
         this.setState({
             addFruitInput: ""
         })
@@ -79,3 +79,102 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, {getFruit, addFruit})(Component)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This is what I'd had before Lucas came over and was rearranging things to try to figure stuff out, but I want to keep what he wrote above because that's the code that he was working with when he finally found the answers to our problem in the reducer file at his home. 
+
+// import React from "react"
+// import {connect} from "react-redux"
+// import {getFruit, addFruit} from "../redux/reducer"
+// import axios from "axios"
+// import IndividualFruit from "./IndividualFruit"
+
+// class Component extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             addFruitInput: ""
+//         }
+//         this.handleAddFruitButtonClick = this.handleAddFruitButtonClick.bind('this')
+//     }
+
+//     componentDidMount(){
+//         axios.get('/api/fruits')
+//         .then(dbResponse => {
+//             console.log("componentDidMount dbResponse.data:", dbResponse.data)
+//             this.props.getFruit(dbResponse.data)
+//         })
+//     }
+
+//     handleInputChange = (e) => {
+//         this.setState({
+//             [e.target.name]: (e.target.value)
+//         })
+//     }
+
+//     handleAddFruitButtonClick (addFruitInput) {
+//         this.props.addFruit(addFruitInput)
+//         this.setState({
+//             addFruitInput: ""
+//         })
+//     }    
+
+//     render(){
+//         // console.log(this.props)
+//         const mappedReduxStateFruitsArray = this.props.fruits.map(individualFruitObject => {
+//             console.log("this is the mappedReduxStateFruitsArray element:", individualFruitObject)
+//             return (
+//                 <IndividualFruit 
+//                     key={individualFruitObject.id} 
+//                     name={individualFruitObject.name}
+//                     id={individualFruitObject.id}
+//                 />
+//             )
+//         })
+//         return (
+//             <div>
+//                 <h1>Favorite Fruits!</h1>
+//                 <div>
+//                 <input 
+//                     name="addFruitInput"
+//                     onChange={e => this.handleInputChange(e)}
+//                     placeholder="Add fruit here"
+//                 ></input>
+//                 <button 
+//                     onClick={() => this.handleAddFruitButtonClick(this.state.addFruitInput)}
+//                 >Add</button>
+//                 </div>
+//                 {mappedReduxStateFruitsArray}
+//             </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = (reduxState) => {
+//     const {fruits} = reduxState.reducer
+//     return {
+//         fruits: fruits
+//     }
+// } 
+
+// const mapDispatchToProps = {
+//     getFruit,
+//     addFruit
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Component)
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const initialState = {
-    fruits: [{id: 5, name: "lemon"}, {id: 8, name: "lime"}]
+    fruits: []
 }
 
 const GET_FRUIT = "GET_FRUIT"
@@ -9,61 +9,121 @@ const ADD_FRUIT = "ADD_FRUIT"
 const EDIT_FRUIT = "EDIT_FRUIT"
 const DELETE_FRUIT = "DELETE_FRUIT"
 
+//I had typed what is immediately below, and it wasn't working, because on the front end I was making a request to dispatch this, which is also a dispatch. So, I was dispatching a dispatch, not a... function?? Idk why but something about this wasn't working. So, Lucas worked on it for a long while, and finally figured out that the thing written after DOES work. 
+
+// export function getFruit () {
+//     axios.get('/api/fruits').then(res => {
+//         return {
+//             type: GET_FRUIT, 
+//             payload: res.data
+//         }
+//     }) 
+// }
+
 export function getFruit () {
-    axios.get('/api/fruits').then(res => {
-        return {
-            type: GET_FRUIT, 
-            payload: res.data
-        }
-    }) 
-}
-
-export function addFruit (fruitToAdd) {
-    console.log("is this a string?", fruitToAdd)
-    axios.post('/api/fruits', {name: `${fruitToAdd}`})//this axios request can only work in a reducer (such as in this case) because you did applyMiddleware(promiseMiddleware) when you created the store in store.js
-    .then(dbResponse => {
-        console.log(dbResponse.data)
-        return {
-            type: ADD_FRUIT, 
-            payload: dbResponse.data
-        }
+    let data = axios.get('/api/fruits').then(res => {
+        return res.data
     })
-} 
-
-export function editFruit(editedName){
     return {
-        type: EDIT_FRUIT, 
-        payload: editedName
+        type: GET_FRUIT, 
+        payload: data
     }
 }
 
-export function deleteFruit(id){
-    console.log("HIT DELETEFRUIT")
-    axios.delete(`/api/fruits/${id}`).then(response => {
-        console.log(response, "response")
-        return {
-            type: DELETE_FRUIT, 
-            payload: response.data
-        }
-    })
+// export function addFruit (fruitToAdd) {
+//     console.log("is this a string?", fruitToAdd)
+//     axios.post('/api/fruits', {name: `${fruitToAdd}`})//this axios request can only work in a reducer (such as in this case) because you did applyMiddleware(promiseMiddleware) when you created the store in store.js
+//     .then(dbResponse => {
+//         console.log(dbResponse.data)
+//         return {
+//             type: ADD_FRUIT, 
+//             payload: dbResponse.data
+//         }
+//     })
+// } 
+
+export function addFruit (fruitToAdd) {
+    // console.log("is this a string?", fruitToAdd)
+    let data = axios.post('/api/fruits', {name: `${fruitToAdd}`})//this axios request can only work in a reducer (such as in this case) because you did applyMiddleware(promiseMiddleware) when you created the store in store.js
+                    .then(res => res.data)
+    return {
+        type: ADD_FRUIT, 
+        payload: data
+    }
 }
 
+// export function editFruit(editedName){
+//     return {
+//         type: EDIT_FRUIT, 
+//         payload: editedName
+//     }
+// }
+
+export function editFruit(id, input){
+    let data = axios.put(`/api/fruits/${id}`, {name: `${input}`})
+                    .then(res => res.data)
+    return {
+        type: EDIT_FRUIT, 
+        payload: data
+    }
+}
+
+// export function deleteFruit(id){
+//     console.log("HIT DELETEFRUIT")
+//     axios.delete(`/api/fruits/${id}`).then(response => {
+//         console.log(response, "response")
+//         return {
+//             type: DELETE_FRUIT, 
+//             payload: response.data
+//         }
+//     })
+// }
+
+​
+export function deleteFruit(id){
+    let data = axios.delete(`/api/fruits/${id}`).then(res => res.data)
+    return {
+        type: DELETE_FRUIT, 
+        payload: data
+    }
+}
+
+// export default function reducer (state = initialState, action) {
+//     switch(action.type){
+//         case GET_FRUIT: 
+//             console.log("reducer.js GET_FRUIT case action.payload spread:", ...action.payload)
+//             return {fruits: [...action.payload]}
+
+//         case ADD_FRUIT: 
+//             console.log("reducer ADD_FRUIT action.payload spread:", ...action.payload)
+//             return {fruits: [...action.payload]}
+
+//         case EDIT_FRUIT: 
+//             return {fruits: [...action.payload]}
+
+//         case DELETE_FRUIT: 
+//             return {fruits: [...action.payload]}
+
+//         default: 
+//             return state
+//     }
+// }
+
+​
 export default function reducer (state = initialState, action) {
     switch(action.type){
-        case GET_FRUIT: 
-            console.log("reducer.js GET_FRUIT case action.payload spread:", ...action.payload)
-            return {fruits: [...action.payload]}
-
+        case GET_FRUIT + '_FULFILLED': 
+            return {fruits: action.payload}
+​
         case ADD_FRUIT + "_FULFILLED": 
-            console.log("reducer ADD_FRUIT action.payload spread:", ...action.payload)
-            return {fruits: [...action.payload]}
-
-        case EDIT_FRUIT: 
-            return {fruits: [...action.payload]}
-
+            return {fruits: action.payload}
+​
+        case EDIT_FRUIT + "_FULFILLED": 
+            return {fruits: action.payload}
+​
         case DELETE_FRUIT + "_FULFILLED": 
-            return {fruits: [...action.payload]}
-
+            return {fruits: action.payload}
+​
         default: 
             return state
     }
